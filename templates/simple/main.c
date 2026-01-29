e9d3ddc575cd4bd05c818163d583cef65b1ece55
jmnuf <jose.m.nufio.0@gmail.com>
2026-01-29 03:00:50 +0100

    Rename simple template's main.c.template to main.c

--- templates/simple/main.c
+++ templates/simple/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-  printf("Hello, World!\n");
+  printf("Hello, world!\n");
   for (int i = 1; i < argc; ++i) {
     printf("Arg %d: %s\n", 1, argv[i]);
   }
